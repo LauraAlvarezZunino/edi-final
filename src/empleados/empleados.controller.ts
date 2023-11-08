db3fb444ddfa653356657a15b536e3467624f013
LauraAlvarezZunino <lauraalvarezzunino@gmail.com>
2023-11-08 16:13:50 -0300

    Arregla errores de identado

--- src/empleados/empleados.controller.ts
+++ src/empleados/empleados.controller.ts
@@ -1,47 +1,51 @@
-import { Body, Controller, Delete, Get, Param, ParseIntPipe, Post, Put } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { EmpleadosService } from './empleados.service';
 import { EmpleadoModel } from './empleadoModel';
 
 @Controller('empleados')
 export class EmpleadosController {
+  constructor(private readonly empleadosService: EmpleadosService) {}
 
-    constructor(private readonly empleadosService: EmpleadosService) {}
+  // Agregar nuevo empleado
+  @Post()
+  agregarEmpleado(@Body() empleado: EmpleadoModel) {
+    return this.empleadosService.agregarEmpleado(empleado);
+  }
 
-    @Post()
-    agregarEmpleado(@Body() empleado: EmpleadoModel) {
-        return this.empleadosService.agregarEmpleado(empleado) ;
-    }
+  // Obtener un empleado por id
+  @Get(':id')
+  getEmpleado(@Param('id', ParseIntPipe) id: number) {
+    return this.empleadosService.getEmpleado(id);
+  }
 
- //obtener info 1 empleado
-    @Get(':id')
-    getEmpleado(@Param('id', ParseIntPipe) id: number) {
-        return this.empleadosService.getEmpleado(id);
-    }
+  // Obtener todos los empleados
+  @Get()
+  public getEmpleados(): Array<EmpleadoModel> {
+    return this.empleadosService.getEmpleados();
+  }
 
-    @Get()
-    public getEmpleados():Array<EmpleadoModel> {
-        return  this.empleadosService.getEmpleados();
-    }
+  // Modificar salario de un empleado
+  @Put(':id/salary')
+  modificarSalary(
+    @Param('id', ParseIntPipe) id: number,
+    @Body('salary') salary: number,
+  ) {
+    return this.empleadosService.modificarSalary(id, salary);
+  }
 
-
-
-    @Put(':id/salary')
-    modificarSalary(@Param('id', ParseIntPipe) id: number, @Body('salary') salary: number) {
-       return this.empleadosService.modificarSalary(id, salary);
-    }
-
-    
-    @Delete(':id')
-    remove(@Param('id', ParseIntPipe) id: number) {
-        console.log(id);
-        return this.empleadosService.eliminarEmpleado(id);
-    }
-
-
-
-
-
-
-
-
+  // Eliminar un empleado por id
+  @Delete(':id')
+  remove(@Param('id', ParseIntPipe) id: number) {
+    console.log(id);
+    return this.empleadosService.eliminarEmpleado(id);
+  }
 }
--- src/empleados/empleados.service.ts
+++ src/empleados/empleados.service.ts
@@ -1,77 +1,67 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { EmpleadoModel } from './empleadoModel';
 
 @Injectable()
 export class EmpleadosService {
-     //Defino la lista de productos
+  //Defino la lista de productos
   private empleados: Array<EmpleadoModel>;
 
   constructor() {
-    this.empleados = new Array<EmpleadoModel>;
+    this.empleados = new Array<EmpleadoModel>();
     //Le agrego 2 empleados iniciales
-    let empleado1 = {
-      "id": 1,
-      "name": "Pedro Escamoso",
-      "cellphone": "5494384729" ,
-      "salary": 124.440
-    }
+    const empleado1 = {
+      id: 1,
+      name: 'Pedro Rodriguez',
+      cellphone: '158674356',
+      salary: 124440,
+    };
     this.empleados.push(empleado1);
 
-    let empleado2 = {
-        "id": 2,
-        "name": "Juan Perez",
-        "cellphone": "5494384729" ,
-        "salary": 120.000
-      }
-      this.empleados.push(empleado2);
-
+    const empleado2 = {
+      id: 2,
+      name: 'Juan Perez',
+      cellphone: '15494384729',
+      salary: 120000,
+    };
+    this.empleados.push(empleado2);
   }
 
-public getEmpleado(id: number) {
+  public getEmpleado(id: number) {
     return this.empleados.find((e) => e.id === id);
   }
 
-public agregarEmpleado (nuevoEmpleado: EmpleadoModel) {
-   
-    
-  //agrego empleado a la lista
+  public agregarEmpleado(nuevoEmpleado: EmpleadoModel) {
+    //agrego empleado a la lista
     this.empleados.push(nuevoEmpleado);
     return nuevoEmpleado;
-   }
+  }
 
-public getEmpleados(): Array<EmpleadoModel> {
+  public getEmpleados(): Array<EmpleadoModel> {
     return this.empleados;
   }
 
-public modificarSalary(id: number, newSalary: number) {
-   let empleado = null;
-   for(let i=0;i<this.empleados.length;i++ ){
-    if (this.empleados[i].id===id){
-      this.empleados[i].salary=newSalary;
-      empleado=this.empleados[i];
+  public modificarSalary(id: number, newSalary: number) {
+    let empleado = null;
+    for (let i = 0; i < this.empleados.length; i++) {
+      if (this.empleados[i].id === id) {
+        this.empleados[i].salary = newSalary;
+        empleado = this.empleados[i];
+      }
     }
-   }
-   return empleado;
+    return empleado;
   }
 
-
   public eliminarEmpleado(id: number) {
-   
     // Guardo el empleado que se quiere eliminar en una variable antes de eliminarlo del array para poder retornarlo
-    const empleadoEliminado =  this.empleados.find((e) => e.id == id);
+    const empleadoEliminado = this.empleados.find((e) => e.id == id);
 
     // Busco el indice en el array de empleados donde se encuentra el empleado que quiero eliminar
-    const index = this.empleados.findIndex(e => e.id === id); 
-
-    // retorno un error si el empleado que se quiere eliminar no existe
-    //if(!index) throw new BadRequestException(' El empleado que desea eliminar no existe');
+    const index = this.empleados.findIndex((e) => e.id === id);
 
     // Remueve el empleado del array
     this.empleados.splice(index, 1);
 
-
     // Retirno el empleado eliminado
     return empleadoEliminado;
   }
- 
 }
